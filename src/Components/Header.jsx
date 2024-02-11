6f593cd5c524f6d19f3527a26b07accef437959e
İlayda Yıldız <146078544+ilaydayilidz778@users.noreply.github.com>
2024-02-11 21:09:54 +0300

    create index.css

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Header
